a5e1f97364252f1e3547593d26ff3fc068ca067c
Luiz Henrique Dalri Fifolato <ldalri1999@gmail.com>
2021-08-17 13:56:09 -0300

    Configurando Classe Startup

--- src/Web/TCC.WebApp.MVC/Configutation/IdentityConfig.cs
+++ src/Web/TCC.WebApp.MVC/Configutation/IdentityConfig.cs
@@ -1,15 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TCC.WebApp.MVC.Configutation
 {
-    public class IdentityConfig
+    public static class IdentityConfig
     {
-        public static void AddIdentityConfiguration(this Iser)
+        public static void AddIdentityConfiguration(this IServiceCollection service)
         {
+            service.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/acesso-negado";
 
+                });
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
         }
     }
 }
---
+++ src/Web/TCC.WebApp.MVC/Configutation/WebAppConfing.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TCC.WebApp.MVC.Configutation
+{
+    public static class WebAppConfing
+    {
+
+        public static void AddMvcConfiguration(this IServiceCollection service)
+        {
+            service.AddControllersWithViews();
+        }
+
+        public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
